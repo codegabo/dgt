83235c2894bd31d680b0cdfe771b92eebd342524
ggabooo <ggabooo@hotmail.com>
2018-05-18 22:23:00 -0500

    la vista del pdf se muestra correctamente en todas las resoluciones, hay que tener en cuenta que cuando se trabaje con php el id llamado desde la base de datos sera llamado en javascript con event.target.id

--- sis.info/js/openPDF.js
+++ sis.info/js/openPDF.js
@@ -1,15 +1,48 @@
 let preview =  document.getElementById('preview-icon'),
-details = document.getElementsByClassName('container-list-files-text');
+    previewsection = document.getElementById('pdf-viewer-container'),
+    iframePDF = document.createElement("IFRAME");
 
 preview.addEventListener('click',previewpdf);
 
 function previewpdf(){
     document.addEventListener("click",function(event) {
-        // let idtarget = event.target.id;
-        if (event.target.id === "preview-icon"){
-            preview.innerHTML = '<iframe src="http://docs.google.com/gview?url=https://www.digitalglobaltextiles.com/recibo.pdf&embedded=true" class="pdf-iframe" frameborder="0"></iframe>';
+        if (event.target.className === "preview-icon" && innerWidth < 1024){
+                // esto es para llamr el id traido desde la base de datos con PHP, la otra linea se deja mientrastanto para hacer pruebas de front end
+                // preview.innerHTML = '<iframe src="http://docs.google.com/gview?url=https://www.digitalglobaltextiles.com/' + event.target.id + 'recibo.pdf&embedded=true" class="pdf-iframe" frameborder="0"></iframe>';
+                preview.innerHTML = '<iframe src="http://docs.google.com/gview?url=https://www.digitalglobaltextiles.com/recibo.pdf&embedded=true" class="pdf-iframe iframe-responsive" id="pdf-iframe" frameborder="0"></iframe>';
         }
-    })
+        if (event.target.className === "preview-icon" && innerWidth >= 1024){
+            previewsection.innerHTML = '<iframe src="http://docs.google.com/gview?url=https://www.digitalglobaltextiles.com/recibo.pdf&embedded=true" class="pdf-iframe" id="pdf-iframe" frameborder="0"></iframe>';
+            // preview.outerHTML = "look for a PDF document";
+            // let newIframe = document.getElementById("pdf-iframe");
+            // newIframe.parentNode.removeChild(newIframe);
+            // return false;
+        }
 
+    })
 }
+addEventListener('resize', previewpdf);
+addEventListener('DOMContentLoaded', previewpdf);
+// el mismo codigo de arriba pero con appenchild
+// let preview =  document.getElementById('preview-icon'),
+//     previewsection = document.getElementById('pdf-viewer-container'),
+//     iframePDF = document.createElement("IFRAME");
+//
+// preview.addEventListener('click',previewpdf);
+//
+// function previewpdf(){
+//     document.addEventListener("click",function(event) {
+//         if (event.target.id === "preview-icon" && innerWidth < 1024){
+//             preview.appendChild(iframePDF);
+//             document.body.appendChild(preview);
+//         } else {
+//             preview.removeChild(iframePDF);
+//             document.body.removeChild(preview);
+//             previewsection.appendChild(iframePDF);
+//             document.body.appendChild(previewsection)
+//         }
+//     })
+// }
+// addEventListener('resize', previewpdf);
+// addEventListener('DOMContentLoaded', previewpdf);
 
