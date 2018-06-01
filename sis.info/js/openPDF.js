c46207f7438dad17a44657f03fb9a9822c10a396
ggabooo <ggabooo@hotmail.com>
2018-06-01 09:46:04 -0500

    se modifico el archivo OpenPDF para que despliegue el pdf de acuerdo al ID pero esta vez sin importar el que escoja

--- sis.info/js/openPDF.js
+++ sis.info/js/openPDF.js
@@ -5,8 +5,9 @@ let preview =  document.getElementById('preview-icon'),
 preview.addEventListener('click',previewpdf);
 
 function previewpdf(){
+    console.log(event.target.id );
     document.addEventListener("click",function(event) {
-        if (event.target.id === "preview-icon" && innerWidth < 1024){
+        if (event.target.id === event.target.id && innerWidth < 1024){
                 // esto es para llamr el id traido desde la base de datos con PHP, la otra linea se deja mientrastanto para hacer pruebas de front end
                 // preview.innerHTML = '<iframe src="http://docs.google.com/gview?url=https://www.digitalglobaltextiles.com/' + event.target.id + 'recibo.pdf&embedded=true" class="pdf-iframe" frameborder="0"></iframe>';
                 preview.innerHTML = '<iframe src="http://docs.google.com/gview?url=https://www.digitalglobaltextiles.com/recibo.pdf&embedded=true" class="pdf-iframe iframe-responsive" id="pdf-iframe" frameborder="0"></iframe>';
